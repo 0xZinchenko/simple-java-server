3672e423ade5bd6a0be7cb43443e60fff404a463
Yan Zinchenko <12407564+YanZinchenko@users.noreply.github.com>
2025-09-20 14:47:09 +0200

    add basic HTTP response handling to async server

--- src/letscode/Main.java
+++ src/letscode/Main.java
@@ -7,6 +7,7 @@ import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -22,28 +23,55 @@ public class Main {
 class Server {
     private final static int BUFFER_SIZE = 256;
     private AsynchronousServerSocketChannel server;
+    private final String HEADERS =
+            "HTTP/1.1 200 OK\r\n" +
+                    "Server: silver\r\n" +
+                    "Content-Type: text/html\r\n" +
+                    "Content-Length: %s\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n";
 
     public void bootstrap() {
         try {
             server = AsynchronousServerSocketChannel.open();
             server.bind(new InetSocketAddress("127.0.0.1", 3000));
 
+            while (true) {
             Future<AsynchronousSocketChannel> future = server.accept();
-            System.out.println("new client thread");
-
-            AsynchronousSocketChannel clientChannel = future.get(30, TimeUnit.SECONDS);
-            while (clientChannel != null && clientChannel.isOpen()) {
-                ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
-
-                clientChannel.read(buffer).get();
-                buffer.flip();
-                clientChannel.write(buffer);
-
-                clientChannel.close();
+                handleClient(future);
             }
-
         } catch (IOException | InterruptedException | ExecutionException | TimeoutException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void handleClient(Future<AsynchronousSocketChannel> future) throws InterruptedException, ExecutionException, TimeoutException, IOException {
+        System.out.println("new client thread");
+
+        AsynchronousSocketChannel clientChannel = future.get(30, TimeUnit.SECONDS);
+        while (clientChannel != null && clientChannel.isOpen()) {
+            ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+            StringBuilder builder = new StringBuilder();
+            boolean keepReading = true;
+
+            while (keepReading) {
+                clientChannel.read(buffer).get();
+
+                int position = buffer.position();
+                keepReading = position == BUFFER_SIZE;
+
+                byte[] array = keepReading ? buffer.array() : Arrays.copyOfRange(buffer.array(), 0, position);
+
+                builder.append(new String(array));
+                buffer.clear();
+            }
+            String body = "<html><body><h1>Hello, silver</h1></body></html>";
+            String page = String.format(HEADERS, body.length()) + body;
+            ByteBuffer resp = ByteBuffer.wrap(page.getBytes());
+            clientChannel.write(resp);
+
+
+            clientChannel.close();
         }
     }
 
