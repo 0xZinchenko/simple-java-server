8d5df43e139b0044b126a713b87054b7d22a92c0
Yan Zinchenko <12407564+YanZinchenko@users.noreply.github.com>
2025-09-20 11:49:12 +0200

    add initial asynchronous server socket setup

--- src/letscode/Main.java
+++ src/letscode/Main.java
@@ -1,11 +1,42 @@
 package letscode;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         new Server();
+
     }
 }
 
 class Server {
+    private AsynchronousServerSocketChannel server;
+
+    public void bootstrap() {
+        try {
+            server = AsynchronousServerSocketChannel.open();
+            server.bind(new InetSocketAddress("127.0.0.1", 3000));
+
+            Future<AsynchronousSocketChannel> future = server.accept();
+            System.out.println("new client thread");
+
+            AsynchronousSocketChannel clientChannel = future.get(30, TimeUnit.SECONDS);
+            while (clientChannel != null && clientChannel.isOpen()) {
+                
+            }
+
+        } catch (IOException | InterruptedException | ExecutionException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
